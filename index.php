7d6f2e570e8d7b9258afa06c4391173f0ca4a0e4
IliaRistars <gillius13579@gmail.com>
2022-02-18 12:31:17 +0200

    fix

--- index.php
+++ index.php
@@ -401,12 +401,15 @@ $membersStr = $Member->get();
       <section class="cabin" id="cabin">
         <div class="container">
           <h2 data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="1000">Cabin Crew</h2>
-          <form class="form" id="cabin-search-form" action="" name="search" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="1000">
+          <div class="form" id="cabin-search-form" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="1000">
             <div class="btn primary search">
-              <button type="button"><svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></svg></button>
+              <button type="button"><svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px">
+                  <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z" />
+                </svg>
+              </button>
               <input type="search" id="search" placeholder="Search cabin crew">
             </div>
-          </form>
+          </div>
           <div class="cabin-crew">
             <div class="crew-wrap">
               <div class="content" id="inmates"></div>
